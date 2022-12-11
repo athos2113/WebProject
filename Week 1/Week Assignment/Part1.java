0d6900e0f17d5b3439b4cfe7657c7a159086eb8c
Junaid Khan <35090434+athos2113@users.noreply.github.com>
2022-12-11 11:51:15 +0530

    Week 1 Assignment

---
+++ Week 1/Week Assignment/Part1.java
@@ -0,0 +1,17 @@
+import edu.duke.*;
+import java.io.File;
+/**
+ * Write a description of Part1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Part1 {
+    
+    public String findSimpleGene (String dna){
+        String result = "";
+        
+        
+        return result; 
+    }
+}
